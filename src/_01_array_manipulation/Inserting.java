d0ca04273853ade9d78f5aecd5428d2064cd1a30
Jsmith2021 <league@WTS7.attlocal.net>
2018-04-01 12:55:19 -0700

    qew
    new

--- src/_01_array_manipulation/Inserting.java
+++ src/_01_array_manipulation/Inserting.java
@@ -1,7 +1,54 @@
 package _01_array_manipulation;
 
-
-
 public class Inserting {
-	
+
+	public static int[] insertAt(int[] testArray, int i, int j) {
+		// TODO Auto-generated method stub
+		for (int j2 = 0; j2 < testArray.length; j2++) {
+			System.out.println(testArray[j2]);
+		}
+		boolean inserted = false;
+		int[] newArray = new int[testArray.length + 1];
+		for (int k = 0; k < i; k++) {
+			newArray[k] = testArray[k];
+		}
+		
+		newArray[i] =  j
+				;
+		for (int k = 0; k < testArray.length; k++) {
+			newArray[k+1] = testArray[i];
+		}
+		for (int j2 = 0; j2 < newArray.length; j2++) {
+			System.out.println(newArray[j2]);
+		}
+		
+		
+		
+		
+		
+		//		for (int k = 0; k < testArray.length; k++) {
+//			if (i == k) {
+//			newArray[k] = j;
+//			inserted = true;
+//			} else {
+//				if(inserted = false) {
+//					newArray[k] = testArray[k];
+//				} else {
+//					if(k > 0) {
+//						newArray[k] = testArray[k-1];
+//					}
+//					
+//				}
+//				
+//			}
+//			
+//		}
+		return newArray;
+
+	}
+
+	public static String[] insertAlphabetically(String[] orderedArray, String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
--- src/_01_array_manipulation/Sorting.java
+++ src/_01_array_manipulation/Sorting.java
@@ -2,4 +2,18 @@ package _01_array_manipulation;
 
 public class Sorting {
 
+	public static void sort(String[] testArray) {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < testArray.length; i++) {
+			for (int j = 0; j < testArray.length; j++) {
+				if (testArray[i].compareTo(testArray[j]) < 0) {
+					String a = testArray[i];
+					testArray[i] = testArray[j];
+					testArray[j] = a;
+
+				}
+			}
+		}
+	}
+
 }
