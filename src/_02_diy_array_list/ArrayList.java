d882fe88a1b2687562638bc3228caa83ab0e6a6e
100297 <league@WTS7.attlocal.net>
2018-04-15 12:29:11 -0700

    awe
    awe

--- src/_02_diy_array_list/ArrayList.java
+++ src/_02_diy_array_list/ArrayList.java
@@ -22,41 +22,55 @@ public class ArrayList<T> {
 
 	public void set(int i, String string) {
 		// TODO Auto-generated method stub
-			ts[i] = (T) string;
+		ts[i] = (T) string;
 	}
 
-	public Object get(int i) {
+	public T get(int i) {
 		// TODO Auto-generated method stub
 
 		return ts[i];
 	}
 
-	public void addAt(T i, T c) {
+	public void addAt(int i, T c) {
 		// TODO Auto-generated method stub
+		T[] ts1 = (T[]) new Object[ts.length + 1];
+		for (int j = 0; j < i; j++) {
+			ts1[j] = ts[j];
+		}
+		ts1[i] = c;
+		for (int j = i + 1; j < ts1.length; j++) {
+			ts1[j] = ts[j - 1];
 
+		}
+		ts = ts1;
 	}
 
-	public boolean contains( String word) {
+	public boolean contains(String word) {
 		for (int i = 0; i < ts.length; i++) {
-			if(ts[i] == word) {
+			if (ts[i] == word) {
 				return true;
 			}
 		}
-		
+
 		return false;
 		// TODO Auto-generated method stub
-			
+
 	}
 
 	public void remove(int i) {
 		// TODO Auto-generated method stub
-		T[] ts1 = (T[]) new Object[ts.length -1];
-		for (int j = 0; j < i-1; j++) {
-			ts1[j] = ts[j];
+		T[] ts1 = (T[]) new Object[ts.length - 1];
+		for (int j = 0; j < ts.length; j++) {
+			if (i != j) {
+				if (j > i) {
+					ts1[j - 1] = ts[j];
+
+				} else {
+					ts1[j] = ts[j];
+				}
+			}
 		}
-		for (int j = i+1; j < ts.length; j++) {
-			ts1[j] = ts[j];
-		}
+
 		ts = ts1;
 	}
 
